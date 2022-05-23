9fc10770bdf9035fb00caef4b869b65415f4312f
sunil <seungiri841@gmail.com>
2022-05-23 04:19:06 -0700

    changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-a = int(input("enter any number"))
-
-if a%2 == 0:
-    print(f'{a} is an even number.')
+a = int(input("enter first number"))
+b = int(input("enter second number"))
+if a>b:
+    print(f'{a} is greatest')
 else:
-    print(f'{a} is an odd number.')
+    print(f'{b} is greatest')

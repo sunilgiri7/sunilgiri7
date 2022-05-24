e29902a4defe42e2784d250281ca50300f3becdf
sunil <seungiri841@gmail.com>
2022-05-24 12:43:09 -0700

    changes

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-while(True):
- print("press q to quite")
- a = input("enter a number:")
-if a == 'q':
-    break
-a = int(a)
-if a>6:
-    print(f'{a} is greatest')
-else:
-    print(f'{b} is greatest')
 
-print("thanks for playing this game")
--- rough.py
+++ rough.py
@@ -1,3 +1,9 @@
-def greet(name):
-    print("good morning","\t"+name)
-greet('sunil')
+def foo(a_list):
+    a_list[2] = 200
+    #print(a_list)
+    a_list.append(4)
+    #a_list[0] = -100
+
+my_list = [1,2,3]
+foo(my_list)
+print(my_list)

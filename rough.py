89df3bbcd3c82979cdf2b93ecd70d51a225e973b
sunil <seungiri841@gmail.com>
2022-05-23 20:31:17 -0700

    changes

--- rough.py
+++ rough.py
@@ -1,11 +1,3 @@
-from http import client
-
-
-try:
-    SellSta= client.get_order(symbol=Symb,orderId=SellOrderNum,recvWindow=Delay)
-except Exception as e:
-    if e.code==-2013:
-        print ("Order does not exist.");
-    elif e.code==-2014:
-        print ("API-key format invalid.");
-    #End If
+def greet(name):
+    print("good morning","\t"+name)
+greet('sunil')

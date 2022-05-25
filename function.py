d4201b01a1f68b266243ac82020473ee050d1735
sunil <seungiri841@gmail.com>
2022-05-25 01:52:56 -0700

    changes

---
+++ function.py
@@ -0,0 +1,18 @@
+def add_numbers(num1,num2):
+    sum = num1 + num2
+    return sum
+
+def multiply_numbers(n1, n2):
+    multiply = n1 * n2
+    return multiply
+
+num1 = 5
+num2 = 6
+sum= add_numbers(num1,num2)
+
+number1 = 4
+number2 = 5
+multiply = multiply_numbers(number1, number2)
+
+print("sum of two numbers is: ",sum)
+print("multiplication of two numbers is: ",multiply)
--- rough.py
+++ rough.py
@@ -1,9 +1,24 @@
-def foo(a_list):
-    a_list[2] = 200
-    #print(a_list)
-    a_list.append(4)
-    #a_list[0] = -100
+def find_average(marks):
+    sum_of_marks = sum(marks)
+    total_subject = len(marks)
+    avg_marks = sum_of_marks / total_subject
+    return avg_marks
 
-my_list = [1,2,3]
-foo(my_list)
-print(my_list)
+#calculate the grade and return it
+def compute_grade(average_marks):
+    if average_marks >= 80:
+        grade = 'A'
+    elif average_marks >= 60:
+        grade = 'B'
+    elif average_marks >= 50:
+        grade = 'C'
+    else:
+        grade = 'fail'
+        return grade
+    
+marks = [55, 64, 75, 80, 65]
+average_marks = find_average(marks)
+print("your average marks is:",average_marks)
+
+grade = compute_grade(average_marks)
+print("your grade is:",grade)
